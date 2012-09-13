6385d35c8cf25a00eaf2cf16b0ca6c9710f110a0
pfiaux <pfiaux@vt.edu>
2012-09-13 22:14:02 +0200

    got l3 working with re

--- level3.py
+++ level3.py
@@ -4,3 +4,16 @@
 
 # use Reg exp to tryyy
 import re
+
+inputfile = open("./level3_data.txt")
+data = ""
+
+#for line in inputfile:
+#	data = data + line.strip()
+
+pattern = "[A-Z][A-Z][A-Z][a-z][A-Z][A-Z][A-Z]"
+
+matches = re.findall(pattern, inputfile.read())
+
+for match in matches:
+	print match
