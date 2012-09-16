11a683498bcf71b26b65d955e073833c0bc1e312
pfiaux <pfiaux@vt.edu>
2012-09-16 12:28:01 +0200

    added new files...

---
+++ level6.py
@@ -0,0 +1,11 @@
+# Python Challenge Level 6
+http://www.pythonchallenge.com/pc/def/channel.html
+
+# Title: now there are pairs
+
+# hint
+# The following has nothing to do with the riddle itself. I just
+# thought it would be the right point to offer you to donate to the
+# Python Challenge project. Any amount will be greatly appreciated.
+# Donnate button
+
