684bdd1c0e11e6bf94ab8ad82f00fbfb0c06ef0e
pfiaux <pfiaux@vt.edu>
2012-09-16 12:27:21 +0200

    solved level 5

--- level5.py
+++ level5.py
@@ -5,3 +5,23 @@
 
 # hint
 # <!-- peak hell sounds familiar ? -->
+# ok got it pickle...
+
+# <peakhell src="banner.p"/>
+# renamed banner.p level5_data.p
+
+inputfile = open("./level5_data.p")
+#data = inputfile.read()
+
+import pprint, pickle
+
+data1 = pickle.load(inputfile)
+inputfile.close()
+#pprint.pprint(data1)
+
+for linedata in data1:
+	line = ""
+	for part in linedata:
+		line += part[0]*part[1]
+	print line
+	
